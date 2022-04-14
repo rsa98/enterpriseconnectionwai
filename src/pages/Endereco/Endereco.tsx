4a6886a11ca05dc38e037ca609861cad48736448
Christian Henrique <christianhmartins@gmail.com>
2022-04-14 02:37:38 -0300

    Página de endereco pronta
    Beleza

--- src/pages/Endereco/Endereco.tsx
+++ src/pages/Endereco/Endereco.tsx
@@ -1,7 +1,14 @@
 import menu from "../../assets/menu.svg";
 import notificacoes from "../../assets/notificacao.svg";
 import profile from "../../assets/profile.jpg";
+import busca from "../../assets/search.svg";
 import "./style.css";
+
+let cep = "12345-678";
+let endereco = "Rua Pedro Borges da Silva";
+let bairro = "Faria Lima";
+let cidade = "Itapevi";
+let estado = "São Paulo";
 
 function Endereco() {
     return (
@@ -16,7 +23,35 @@ function Endereco() {
           </div>
         </header>
         <main>
-          esta é a main
+          <form action="" id="buscar-cep">
+            <input type="text" placeholder="Insire seu cep" maxLength={8}  required/>
+            <button>
+              <img src={busca} alt="Buscar" />
+            </button>
+          </form>
+          <div id="resultado-busca">
+            <h1>Endereço</h1>
+            <div className="resultado">
+              <h2>CEP</h2>
+              <p>{cep}</p>
+            </div>
+            <div className="resultado">
+              <h2>Endereço</h2>
+              <p>{endereco}</p>
+            </div>
+            <div className="resultado">
+              <h2>Bairro</h2>
+              <p>{bairro}</p>
+            </div>
+            <div className="resultado">
+              <h2>Cidade</h2>
+              <p>{cidade}</p>
+            </div>
+            <div className="resultado">
+              <h2>Estado</h2>
+              <p>{estado}</p>
+            </div>
+          </div>
         </main>
       </div>
     )
